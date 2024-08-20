f0a99de6e9ee2a61c7c01ff9a2d7f65e8625ffd9
ltranc <84255102+ltranc@users.noreply.github.com>
2024-08-19 22:08:37 -0400

    Update gui.js

--- js/gui.js
+++ js/gui.js
@@ -281,7 +281,7 @@ $(document).ready(function() {
 			// Growths
 			var growthRow = $("<tr/>");
 			growthRow.append($("<td/>").append($("<span/>").addClass("growth-td").text("Grow")));
-			for (var attr in levelList[i][0].statCap)
+			for (var attr in levelList[i][0].growth)
 				growthRow.append($("<td/>").append($("<span/>").addClass("growth-td").text(levelList[i][0].growth[attr] + "%")));
 			tableBody.append(growthRow);
 			
