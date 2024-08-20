2e34668c4c0329c40d020326a7e309c9b6dff1c0
ltranc <84255102+ltranc@users.noreply.github.com>
2024-08-20 00:02:48 -0400

    Update statCalc.js

--- js/statCalc.js
+++ js/statCalc.js
@@ -263,8 +263,8 @@ StatCalculator.prototype.compute = function() {
 				// Does not grow if stat is at cap
 				// The extra multiplication eliminates javascript floating point precision problem
 				thisLevel.statCap[attr] = prev.statCap[attr];
-				thisLevel.growths[attr] = prev.growths[attr];
-				thisLevel.stat[attr] = Math.min((prev.stat[attr]*FIX + growth*FIX/100)/FIX, thisLevel.statCap[attr]);	
+				thisLevel.stat[attr] = Math.min((prev.stat[attr]*FIX + growth*FIX/100)/FIX, thisLevel.statCap[attr]);
+				thisLevel.growths[attr] = growth
 			}
 		}
 		prev = thisLevel;
